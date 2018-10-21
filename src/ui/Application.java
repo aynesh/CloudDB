af0f5271fe76f7cb3b03830e4951becdcd984281
Akshaya Muralidharan <ga53juk@mytum.de>
2018-10-21 20:41:11 +0200

    functionalitites+logger comments

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -9,42 +9,45 @@ import org.apache.log4j.BasicConfigurator;
 
 public class Application {
 
-	static Logger logger = Logger.getLogger(Application.class);
-	
 	public static void main(String[] args) throws IOException {
-		logger.info("Hello World");     
+		   
 		BufferedReader cons = new BufferedReader(new
-		InputStreamReader(System.in));
+				InputStreamReader(System.in));
 		boolean quit = false;
+		
 		Client client = new Client();
+		
 		while (!quit) {
-		System.out.print("Client> ");
-		String input = cons.readLine();
-		String[] tokens = input.trim().split("\\s+");
-		if(tokens[0].equals("quit")) {
-			quit = true;
-		}
-		else if(tokens[0].equals("connect")) {
-			client.connect(tokens[1], tokens[2]);
-		}
-		else if(tokens[0].equals("send")) {
-			client.send(tokens[1]);
-		}
-		else if(tokens[0].equals("disconnect")) {
-			client.disconnect();
-		}
-		else if(tokens[0].equals("help")) {
-			client.help();
-		}
-		else if(tokens[0].equals("logLevel")) {
-			client.setLevel(tokens[1]);
+			System.out.print("Client> ");
+			String input = cons.readLine();
+			String[] tokens = input.trim().split("\\s+");
+			if(tokens[0].equals("quit")) {
+				quit = true;
+			}
+			else if(tokens[0].equals("connect")) {
+				client.connect(tokens[1], tokens[2]);
+			}
+			else if(tokens[0].equals("send")) {
+				client.send(tokens[1]);
+			}
+			else if(tokens[0].equals("disconnect")) {
+				client.disconnect();
+			}
+			else if(tokens[0].equals("help")) {
+				client.help();
+			}
+			else if(tokens[0].equals("logLevel")) {
+				client.setLevel(tokens[1]);
+
+			}
+			else {
+				client.invalidCommand();
+			}
 			
 		}
-		else {
-			client.invalidCommand();
-		}
 		
-		}
-		}
+		client.terminate();
+
+	}
 
 }
--- src/ui/Client.java
+++ src/ui/Client.java
@@ -1,12 +1,17 @@
 package ui;
 
-import java.io.IOException;
+import java.io.*;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public class Client {
+	
+	static Logger logger = Logger.getLogger(Client.class);
 	
 	Socket sock;
 	InputStream in;
@@ -14,15 +19,27 @@ public class Client {
 
 	public void connect(String ip, String port) {
 		try {
+			//connect 131.159.52.2 50000
 			sock = new Socket(ip, Integer.parseInt(port));
 			in = sock.getInputStream();
 			out = sock.getOutputStream();
+			
+			byte[] recvBytes = new byte[131072];
+			in.read(recvBytes);
+			System.out.println("Server> ");
+			for(int i=0;recvBytes[i]!='\n';i++) {
+				System.out.print((char)recvBytes[i]);
+			}
+			System.out.println();
+			
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Unknown host. Unable to establish connection.");
+			logger.error("Unknown host. Unable to establish connection.");
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Unable to establish connection.");
+			logger.error("Unable to establish connection.");
+
 		}
 	}
 	public void disconnect() {
@@ -31,7 +48,10 @@ public class Client {
 			in.close();
 			out.close();
 			sock.close();
+			System.out.println("Diconnected from server.");
 		} catch (IOException e) {
+			System.out.println("Diconnect failed.");
+
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -39,28 +59,49 @@ public class Client {
 	}
 	public void send(String tokens) {
 	
+		if(sock.isClosed()) {
+			System.out.println("No open connection to server.");
+		}
+		else {
 		try {
-			
+			tokens = tokens + "\r";
 			out.write(tokens.getBytes());
 			out.flush();
 			
-			byte[] recvByte = null;
-			in.read(recvByte);
+			byte[] recvBytes = new byte[131072];
+			in.read(recvBytes);
+			System.out.println("Server> ");
+			for(int i=0;recvBytes[i]!='\n';i++) {
+				System.out.print((char)recvBytes[i]);
+			}
+			System.out.println();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} 
 		}
 
 	}
 	
 	public void setLevel(String level) {
+		logger.info("Previous level:"+logger.getLevel());
+		logger.setLevel(Level.toLevel(level));
+		logger.info("New level:"+logger.getLevel());
 		
 	}
 	public void help() {
 		
 	}
 	public void invalidCommand() {
+		System.out.println("Unknown command.");
+
 		
+	}
+	public void terminate() {
+		if(sock.isConnected()) {
+			disconnect();
+		}
+		System.out.println("Client terminated.");
 		
 	}
 }
