fb9c27ddf0f4de034f37b9e7cc284972afc5cdd8
Akshaya Muralidharan <ga53juk@mytum.de>
2018-10-22 22:24:27 +0200

    javadocs + exceptions

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -8,46 +8,73 @@ import java.util.Arrays;
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
+public class Application
+{
 
-public class Application {
+	/**
+	 * @param args Unused.
+	 * @return Nothing.
+	 * @exception IOException On input error.
+	 * @throws IOException
+	 * this main method creates an instance of Client class,
+	 * receives commands through BufferedReader as Strings
+	 * and behaves accordingly. 
+	 */
+	public static void main(String[] args) throws IOException
+	{
 
-	public static void main(String[] args) throws IOException {
-		   
-		BufferedReader cons = new BufferedReader(new
-				InputStreamReader(System.in));
+		BufferedReader cons = new BufferedReader(new InputStreamReader(System.in));
 		boolean quit = false;
-		
+
 		Client client = new Client();
-		
-		while (!quit) {
+
+		while (!quit)
+		{
 			System.out.print("Client> ");
 			String input = cons.readLine();
 			String[] tokens = input.trim().split("\\s+");
-			if(tokens[0].equals("quit")) {
+			if (tokens[0].equals("quit"))
+			{
 				quit = true;
 			}
-			else if(tokens[0].equals("connect")) {
+			else if (tokens[0].equals("connect"))
+			{
+				if(tokens.length<3) {
+					System.out.println("Incorrect usage of command.");
+					client.help();
+				}
+				else {
 				client.connect(tokens[1], tokens[2]);
+				}
 			}
-			else if(tokens[0].equals("send")) {
+			else if (tokens[0].equals("send"))
+			{
 				client.sendMessage(String.join(" ", Arrays.copyOfRange(tokens, 1, tokens.length)));
 			}
-			else if(tokens[0].equals("disconnect")) {
+			else if (tokens[0].equals("disconnect"))
+			{
 				client.disconnect();
 			}
-			else if(tokens[0].equals("help")) {
+			else if (tokens[0].equals("help"))
+			{
 				client.help();
 			}
-			else if(tokens[0].equals("logLevel")) {
+			else if (tokens[0].equals("logLevel"))
+			{
+				if(tokens.length<2) {
+					System.out.println("Incorrect usage of command.");
+					client.help();
+				}
+				else {
 				client.setLevel(tokens[1]);
-
+				}
 			}
-			else {
+			else
+			{
 				client.invalidCommand();
 			}
-			
 		}
-		
+
 		client.terminate();
 
 	}
--- src/ui/Client.java
+++ src/ui/Client.java
@@ -3,33 +3,36 @@ package ui;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Arrays;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class Client {
-	
+
 	static Logger logger = Logger.getLogger(Client.class);
-	
+
 	Socket sock;
 	InputStream in;
 	OutputStream out;
 
+	/**
+	 * @param ip
+	 * @param port This method creates connection to the given address represented
+	 *             by 2 parameters: ip and port;
+	 */
 	public void connect(String ip, String port) {
 		try {
-			//connect 131.159.52.2 50000
-			logger.info("Attempting to establish connection to "+ip+" "+port);
+			// connect 131.159.52.2 50000
+			logger.info("Attempting to establish connection to " + ip + " " + port);
 
 			sock = new Socket(ip, Integer.parseInt(port));
 			in = sock.getInputStream();
 			out = sock.getOutputStream();
-			
+
 			byte[] recvBytes = new byte[131072];
 			in.read(recvBytes);
 			String recd = new String(recvBytes, "UTF-8").trim();
-			System.out.print("Server> "+recd);
-			logger.info("Server response: "+recd);
+			System.out.println("Server> " + recd);
+			logger.info("Server response: " + recd);
 
 		} catch (UnknownHostException e) {
 			System.out.println("Unknown host. Unable to establish connection.");
@@ -41,15 +44,19 @@ public class Client {
 
 		}
 	}
+
+	/**
+	 * This method checks if there is a connection and if yes, it shuts down the
+	 * active connection.
+	 */
 	public void disconnect() {
-		if(sock == null || sock.isClosed()) {
+		if (sock == null || sock.isClosed()) {
 			System.out.println("No open connection.");
 			logger.error("No open connection.");
 			return;
 		}
-		
+
 		try {
-	
 			in.close();
 			out.close();
 			sock.close();
@@ -59,18 +66,23 @@ public class Client {
 			System.out.println("Diconnect failed.");
 			logger.error("Failed to disconnect.");
 		}
-		
+
 	}
+
+	/**
+	 * @param tokens this method checks whether a socket is closed/is empty and
+	 *               sends warning if yes, or sends the byte array otherwise. (calls
+	 *               send method)
+	 */
 	public void sendMessage(String tokens) {
-	
-		if(sock == null || sock.isClosed()) {
+
+		if (sock == null || sock.isClosed()) {
 			logger.warn("Attempting to connect via closed socket.");
 			System.out.println("No open connection to server.");
-			
-		}
-		else {
-		
-			logger.info("Attempting to send message: "+tokens);
+
+		} else {
+
+			logger.info("Attempting to send message: " + tokens);
 			tokens = tokens + "\r";
 			send(tokens.getBytes());
 			receive();
@@ -78,18 +90,27 @@ public class Client {
 		}
 
 	}
-	
+
+	/**
+	 * @param level This method sets the level of logging according to priority
+	 */
 	public void setLevel(String level) {
 		Level prvLevel = logger.getLevel();
-		
+
 		logger.setLevel(Level.toLevel(level));
-		System.out.println("Changed log level from "+prvLevel+" to "+ logger.getLevel());
-		logger.info("Changed log level from "+prvLevel+" to "+ logger.getLevel());
-		
+		System.out.println("Changed log level from " + prvLevel + " to " + logger.getLevel());
+		logger.info("Changed log level from " + prvLevel + " to " + logger.getLevel());
+
 	}
+
+	/**
+	 * This method lists all the possible available actions that can be performed by
+	 * a user
+	 */
 	public void help() {
 		System.out.println("Intended usage of available commands:");
-		System.out.println("connect <address> <port> - Tries to establish a TCP- connection to the server at <address> and <port>.");
+		System.out.println(
+				"connect <address> <port> - Tries to establish a TCP- connection to the server at <address> and <port>.");
 		System.out.println("disconnect - Tries to disconnect from server");
 		System.out.println("send <message> - Tries to send <message> to connected server");
 		System.out.println("logLevel <level> - Tries to set logger level to <level>");
@@ -97,20 +118,29 @@ public class Client {
 		System.out.println("help - displays list of available commands");
 	}
 
+	/**
+	 * This method notifies user in case the latter calls unknown command
+	 */
 	public void invalidCommand() {
 		System.out.println("Unknown command.");
 		help();
 
-		
 	}
+
+	/**
+	 * This method checks if a socket is closed and if it's not, disconnects the
+	 * connection with disconnect() method.
+	 */
 	public void terminate() {
-		if(!sock.isClosed()) {
+		if (!sock.isClosed()) {
 			disconnect();
 		}
 		System.out.println("Client terminated.");
-		
 	}
-	
+
+	/**
+	 * @param arr this method sends an array of bytes via OutputStream
+	 */
 	public void send(byte[] arr) {
 		try {
 			out.write(arr);
@@ -119,26 +149,28 @@ public class Client {
 			System.out.println("Failed to send message.");
 			logger.error("Failed to send message.");
 		}
-		
 	}
-	
+
+	/**
+	 * @return byte[] This method receives an array of bytes and creates a String
+	 *         out of that array.
+	 */
 	public byte[] receive() {
 		byte[] recvBytes = new byte[131072];
 		try {
 			in.read(recvBytes);
 			String recd;
-			
-				recd = new String(recvBytes, "UTF-8").trim();
-				System.out.println("Server> "+recd);
-				logger.info("Server response: "+recd);
-			} catch (UnsupportedEncodingException e) {
-				System.out.println("Failed to decode message.");
-				logger.error("Failed to decode message.");
-			} catch (IOException e) {
-				System.out.println("Failed to receive response.");
-				logger.error("Failed to receive response.");
-			}	
-		
-		return recvBytes;		
+
+			recd = new String(recvBytes, "UTF-8").trim();
+			System.out.println("Server> " + recd);
+			logger.info("Server response: " + recd);
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("Failed to decode message.");
+			logger.error("Failed to decode message.");
+		} catch (IOException e) {
+			System.out.println("Failed to receive response.");
+			logger.error("Failed to receive response.");
+		}
+		return recvBytes;
 	}
 }
